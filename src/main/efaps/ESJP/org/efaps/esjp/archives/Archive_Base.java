08b296b15bdd6267260185af97bcdf0ac2b228ed
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2011-08-18 23:30:34 +0000

    - adding method to upload zip archives to archive
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/archives/trunk@6979 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/archives/Archive_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/archives/Archive_Base.java
@@ -20,7 +20,18 @@
 
 package org.efaps.esjp.archives;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.efaps.admin.datamodel.Type;
 import org.efaps.admin.event.Parameter;
@@ -29,17 +40,19 @@ import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Context;
 import org.efaps.db.Insert;
 import org.efaps.db.Instance;
 import org.efaps.db.MultiPrintQuery;
 import org.efaps.db.QueryBuilder;
 import org.efaps.esjp.ci.CIArchives;
+import org.efaps.esjp.common.file.FileUtil;
 import org.efaps.esjp.common.uiform.Create;
 import org.efaps.util.EFapsException;
 
 /**
  * TODO description!
- * 
+ *
  * @author The eFasp Team
  * @version $Id: TreeViewStructurBrowser_Base.java 5979 2010-12-23 03:37:33Z
  *          jan@moxter.net $
@@ -50,7 +63,7 @@ public abstract class Archive_Base
 {
     /**
      * Create a new Archive.
-     * 
+     *
      * @param _parameter Parameter as passed by the eFaps API
      * @return new Return
      * @throws EFapsException on error
@@ -80,7 +93,7 @@ public abstract class Archive_Base
 
     /**
      * Create a Root Folder.
-     * 
+     *
      * @param _parameter Parameter as passed by the eFaps API
      * @return new Return
      * @throws EFapsException on error
@@ -94,6 +107,12 @@ public abstract class Archive_Base
         return new Return();
     }
 
+    /**
+     * Check if access will be granted to the cmd to create a root node.
+     * @param _parameter    Parameter as passed by the eFaps API
+     * @return new Return
+     * @throws EFapsException on error
+     */
     public Return checkAccess2ArchiveRoot(final Parameter _parameter)
         throws EFapsException
     {
@@ -116,13 +135,129 @@ public abstract class Archive_Base
                 }
             }
         }
-
         return ret;
     }
 
+
+    /**
+     * Check if access will be granted to the cmd to create a root node.
+     *
+     * @param _parameter    Parameter as passed by the eFaps API
+     * @return new Return
+     * @throws EFapsException on error
+     */
     public Return createFromZip(final Parameter _parameter)
         throws EFapsException
     {
+        final Context.FileParameter fileItem = Context.getThreadContext().getFileParameters().get("upload");
+        if (fileItem != null) {
+            try {
+                final InputStream input = fileItem.getInputStream();
+                final ZipInputStream zis = new ZipInputStream(new BufferedInputStream(input));
+                ZipEntry entry;
+                final FileUtil fileUtil = new FileUtil();
+                final List<File> files = new ArrayList<File>();
+                while ((entry = zis.getNextEntry()) != null) {
+                    int size;
+                    final byte[] buffer = new byte[2048];
+                    final File file = fileUtil.getFile(entry.getName());
+                    if (!file.isHidden()) {
+                        files.add(file);
+                        final FileOutputStream fos = new FileOutputStream(file);
+                        final BufferedOutputStream bos = new BufferedOutputStream(fos, buffer.length);
+                        while ((size = zis.read(buffer, 0, buffer.length)) != -1) {
+                            bos.write(buffer, 0, size);
+                        }
+                        bos.flush();
+                        bos.close();
+                    }
+                }
+                for (final File file : files) {
+                    if (file.length() > 0) {
+                        final Context.FileParameter fileTmp = new FileItem(file);
+                        Context.getThreadContext().getFileParameters().put("upload", fileTmp);
+                        _parameter.getParameters().put("name", new String[]{ fileTmp.getName() });
+                        create(_parameter);
+                    }
+                }
+            } catch (final IOException e) {
+                throw new EFapsException(Archive_Base.class, "createFromZip", e);
+            }
+        }
         return new Return();
+    }
+
+
+    public class FileItem
+        implements Context.FileParameter
+    {
+        /**
+         * File this item belongs to.
+         */
+        private final File file;
+
+        /**
+         * @param _file
+         */
+        public FileItem(final File _file)
+        {
+            this.file = _file;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void close()
+            throws IOException
+        {
+            //nothing must be done
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public InputStream getInputStream()
+            throws IOException
+        {
+            return new FileInputStream(this.file);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public long getSize()
+        {
+            return this.file.length();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String getContentType()
+        {
+            return "";
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String getName()
+        {
+            return this.file.getName();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String getParameterName()
+        {
+            return "upload";
+        }
     }
 }
